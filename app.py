47add4d4e8fa84564e279ecc5b69a40041798372
Carlosmicael <carlosmlo381@gmail.com>
2025-04-17 10:09:33 -0500

    logica arreglada

--- app.py
+++ app.py
@@ -6,29 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     getServidorTxt = requests.get("https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt")
-    encabezado = any
     personas_fil = []
-    txtPlano = any
     searchIds = ("3", "4", "5", "7")
 
+    txtPlano = getServidorTxt.text if getServidorTxt.status_code == 200 else ""
 
-    if getServidorTxt.status_code == 200:
-        txtPlano=getServidorTxt.text
-    else:
-        txtPlano = ""
-    
     lineas = txtPlano.strip().split("\n")
-    encabezado = lineas[0].split("|") #aqui sacamos los encabezados
-    datos = lineas[1:] #aqui nos devuelve todos los registros sin los encabezados le decimos coje todas las filas.
-                       #empezando desde la fila 1 no la 0 que serian los encabezados.
+    encabezado = lineas[0].split("|")
+    datos = lineas[1:]
 
-    
     for linea in datos:
-        campos = linea.split("|") #en cada linea como tenemos varios campos los separamos con | es decir id|nombre| etc.
-        if campos[0].startswith(searchIds): #aqui buscamos desde el primer campo es decir id| todos los que comiensen con 3,4,5,7.
-            personas_fil.append(campos) #con el append agregamos a la lista todos los que cumplen con la condicion.
+        campos = linea.split("|")
+        if any(campos[0].startswith(i) for i in searchIds):
+            personas_fil.append(campos)
 
-    #modelamos la tabla y aplicamos css.
     tabla = """
     <style>
         table {
@@ -58,9 +49,6 @@ def home():
         th:nth-child(3), td:nth-child(3) { background-color: #34A853; color: white; }
         th:nth-child(4), td:nth-child(4) { background-color: #4285F4; color: white; }
         th:nth-child(5), td:nth-child(5) { background-color: #A142F4; color: white; }
-        h2{
-            background-color:white;
-        }
     </style>
     <table>
         <tr>
@@ -71,6 +59,6 @@ def home():
 
     tabla += "</table>"
     return tabla
-   
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
